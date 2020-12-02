c49520be4cf86f7839262c9f318667ca45c74587
blockjoe <69400937+blockjoe@users.noreply.github.com>
2020-12-02 18:08:54 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rosetta-api-client-python',
+    author='AUTHOR',
+    author_email='AUTHOR_EMAIL',
+    description='A python client for interacting with Rosetta endpoints',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blockjoe/rosetta-api-client-python",
+    packages=setuptools.find_packages(),
+    license='Apache License 2.0',
+    python_requires='>=3.7',
+    install_requires=[
+        'click',
+        'marshmallow',
+        'aiohttp[speedups]'
+    ]
+)
