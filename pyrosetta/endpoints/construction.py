a59736d2d9c199728c618a82cf3b2925676201eb
gretha <gretha.dossantos@gmail.com>
2020-12-21 14:35:02 -0700

    all endpoint completed

--- pyrosetta/endpoints/construction.py
+++ pyrosetta/endpoints/construction.py
@@ -1,63 +1,79 @@
-def create_network_transaction_from_signatures():
+def create_network_transaction_from_signatures(req : ConstructionCombineRequest):
     """
     req: ConstructionCombineRequest
     resp: ConstructionCombineResponse
     ref: /construction/combine
     """
-    pass
+    url = '{}/construction/combine'.format(API_URL)
+    resp = post_request(url, req.json())
+    return ConstructionCombineResponse(**resp.json())
 
-def derive_account_id_from_pubkey():
+def derive_account_id_from_pubkey(req : ConstructionDeriveRequest):
     """
     req: ConstructionDeriveRequest
     resp: ConstructionDeriveResponse
     ref: /construction/derive
     """
-    pass
+    url = '{}/construction/derive'.format(API_URL)
+    resp = post_request(url, req.json())
+    return ConstructionDeriveResponse(**resp.json())
 
-def get_hash_of_signed_transaction():
+def get_hash_of_signed_transaction(req : ConstructionHashRequest):
     """
     req: ConstructionHashRequest
     resp: TransactionIdentifierResponse
     ref: /construction/hash
     """
-    pass
+    url = '{}/construction/hash'.format(API_URL)
+    resp = post_request(url, req.json())
+    return TransactionIdentifierResponse(**resp.json())
 
-def get_metadata_for_transaction_construction():
+def get_metadata_for_transaction_construction(req : ConstructionMetadataRequest):
     """
     req: ConstructionMetadataRequest
     resp: ConstructionMetadataResponse
     ref: /construction/metadata
     """
-    pass
+    url = '{}/construction/metadata'.format(API_URL)
+    resp = post_request(url, req.json())
+    return ConstructionMetadataResponse(**resp.json())
 
-def parse_transaction():
+def parse_transaction(req : ConstructionParseRequest):
     """
     req: ConstructionParseRequest
     resp: ConstructionParseResponse
     ref: /construction/parse
     """
-    pass
+    url = '{}/construction/parse'.format(API_URL)
+    resp = post_request(url, req.json())
+    return ConstructionParseResponse(**resp.json())
 
-def generate_unsigned_transaction_and_signing_payloads():
+def generate_unsigned_transaction_and_signing_payloads(req : ConstructionPayloadsRequest):
     """
     req: ConstructionPayloadsRequest
     resp: ConstructionPayloadsResponse
     ref: /construction/payloads
     """
-    pass
+    url = '{}/construction/payloads'.format(API_URL)
+    resp = post_request(url, req.json())
+    return ConstructionPayloadsResponse(**resp.json())
 
-def create_request_to_fetch_metadata():
+def create_request_to_fetch_metadata(req : ConstructionPreprocessRequest):
     """
     req: ConstructionPreprocessRequest
     resp: ConstructionPreprocessResponse
     ref: /construction/preprocess
     """
-    pass
+    url = '{}/construction/preprocess'.format(API_URL)
+    resp = post_request(url, req.json())
+    return ConstructionPreprocessResponse(**resp.json())
 
-def submit_signed_transaction():
+def submit_signed_transaction(req : ConstructionSubmitRequest):
     """
     req: ConstructionSubmitRequest
     resp: TransactionIdentifierResponse
     ref: /construction/submit
     """
-    pass
+    url = '{}/construction/submit'.format(API_URL)
+    resp = post_request(url, req.json())
+    return TransactionIdentifierResponse(**resp.json())
--- pyrosetta/endpoints/indexer.py
+++ pyrosetta/endpoints/indexer.py
@@ -1,15 +1,19 @@
-def get_range_of_block_events():
+def get_range_of_block_events(req : EventsBlocksRequest):
     """
     req: EventsBlocksRequest
     resp: EventsBlocksResponse
     ref: /events/blocks
     """
-    pass
+    url = '{}/events/blocks'.format(API_URL)
+    resp = post_request(url, req.json())
+    return EventsBlocksResponse(**resp.json())
 
-def search_for_transactions():
+def search_for_transactions(req : SearchTransactionsRequest):
     """
     req: SearchTransactionsRequest
     resp: SearchTransactionsResponse
     ref: /search/transactions
     """
-    pass
+    url = '{}/search/transactions'.format(API_URL)
+    resp = post_request(url, req.json())
+    return SearchTransactionsResponse(**resp.json())
