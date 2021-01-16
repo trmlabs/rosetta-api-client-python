94366c0e76d02e9bd22b073bb49dba9d31a62319
blockjoe <69400937+blockjoe@users.noreply.github.com>
2021-01-15 20:14:09 -0500

    Handled reserved keyword aliases

--- build-schemas.sh
+++ build-schemas.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-datamodel-codegen --input api.json --output pyrosetta/models/_models.py --field-constraints
+datamodel-codegen --input api.json --output pyrosetta/models/_models.py --field-constraints --aliases aliases.json
